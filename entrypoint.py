1b5ed03b60f236a73f809e00750a18d826750795
Mohammad <mohammad@britecore.com>
2020-04-24 13:26:16 +0300

    Ability to update flow states in Rally team boards

--- entrypoint.py
+++ entrypoint.py
@@ -3,26 +3,40 @@
 import os
 
 from pyral import Rally
-from pyral.restapi import RallyRESTAPIError
 
 class RallyBase:
     def __init__(self, API_KEY):
         self.rally = Rally(apikey=API_KEY)
 
     def get_rally_item(self, item_id, item_type):
-        try:
-            return self.rally.get(item_type, fetch=True, instance=True, projectScopeDown=True, query=f'FormattedID = {item_id}')
-        except RallyRESTAPIError:
-            raise RallyLookupFailure('Failed to find the Story/Defect in Rally')
+        return self.rally.get(item_type, fetch=True, instance=True, projectScopeDown=True, query=f'FormattedID = {item_id}')
 
     def update_item(self, item, item_data):
-        try:
-            return self.rally.update(item._type,
-                                fetch=True,
-                                instance=True,
-                                itemData=item_data)
-        except RallyRESTAPIError:
-            raise RallyLookupFailure('Failed to find the Story/Defect in Rally')
+        return self.rally.update(item._type,
+                            fetch=True,
+                            instance=True,
+                            itemData=item_data)
+
+
+    def get_flow_states(self, project_name):
+        return self.rally.get('FlowState',
+                        project=project_name)
+
+    def get_flow_state(self, project_name, state_name):
+        flow_states = self.get_flow_states(project_name)
+        found = None
+        for flow_state in flow_states:
+            if flow_state.Name == state_name:
+                return flow_state
+        raise LookupError(f'Could not find desired Flow State: {state_name}')
+
+    def normalize_data(self, item, item_data):
+        if item_data.get('FlowStateName'):
+            project_name = item.Project.Name
+            state_name = item_data.pop('FlowStateName')
+            flow_state = self.get_flow_state(project_name, state_name)
+            item_data['FlowState'] = flow_state.oid
+        return item_data
 
 
 def parse_key_value(values):
@@ -48,11 +62,11 @@ def main():
     response = rally.get_rally_item(item_id, item_type)
     parsed_values = parse_key_value(values)
 
-    item_data = {
+    item_data = rally.normalize_data(response, {
         'ObjectID': response.ObjectID,
         'FormattedID': response.FormattedID,
         **parsed_values
-    }
+    })
 
     # Update Rally Item
     rally.update_item(response, item_data)	
